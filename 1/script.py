645e3af79bea609b0ee4b41c5916a5c13896779a
MyGg29 <gwendalgourmelon@gmail.com>
2020-12-25 19:19:35 +0100

    first day part one

---
+++ 1/script.py
@@ -0,0 +1,13 @@
+with open("in") as fIn: 
+   expenseReport = fIn.read().split("\n")
+# cast every items to int so we can work peacefully
+expenseReport = [int(i) for i in expenseReport]
+
+res = 0
+for expenseLeft in expenseReport:
+   for expenseRight in expenseReport:
+      expenseSum = expenseLeft + expenseRight
+      if expenseSum == 2020:
+         res = expenseLeft * expenseRight
+
+print(res)

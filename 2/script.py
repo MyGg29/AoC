234fe86bf8adc782a8c3dc3ef708e61630168e9a
MyGg29 <gwendalgourmelon@gmail.com>
2020-12-26 15:21:04 +0100

    day two part two

--- 2/script.py
+++ 2/script.py
@@ -13,3 +13,16 @@ for passwordRule in passwords:
         res += 1
 
 print(res)
+
+#part two
+res = 0
+for passwordRule in passwords:
+    rule, letter, password = passwordRule.split(" ")
+    #remove the `:` after the letter
+    letter = letter[0]
+    index1, index2 = rule.split("-")
+
+    passwordArray = [char for char in password]
+    if (passwordArray[int(index1) - 1] == letter) != (passwordArray[int(index2) - 1] == letter):
+        res += 1
+print(res)

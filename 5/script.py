ed7003ae8bb27ae96ee665d02a3adb689bd26a9c
MyGg29 <gwendalgourmelon@gmail.com>
2020-12-27 14:36:19 +0100

    day 5 part one

---
+++ 5/script.py
@@ -0,0 +1,39 @@
+import math
+with open("in") as fIn: 
+   seats = fIn.read().split("\n")
+
+def getRow(seat):
+    min = 0
+    max = 127
+    for i in range(7):
+        direction = seat[i]
+        if direction == "F":
+            max = max - math.ceil((max-min)/2)
+        if direction == "B":
+            min = min + math.ceil((max-min)/2)
+    return min
+
+def getCol(seat):
+    min = 0
+    max = 7
+    for i in range(7,10):
+        direction = seat[i]
+        if direction == "L":
+            max = max - math.ceil((max-min)/2)
+        if direction == "R":
+            min = min + math.ceil((max-min)/2)
+    return min
+
+
+def getSeatId(seat):
+    row = getRow(seat)
+    col = getCol(seat)
+    return row * 8 + col
+
+max = 0
+for seat in seats:
+    seatId = getSeatId(seat)
+    if seatId > max:
+        max = seatId
+
+print(max)

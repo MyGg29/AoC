4472cff2acd54938636cffb844093e966a9f520d
MyGg29 <gwendalgourmelon@gmail.com>
2020-12-29 14:36:34 +0100

    day 7 part one

---
+++ 7/script.py
@@ -0,0 +1,40 @@
+with open("in") as fIn: 
+   bags = fIn.read().split("\n")
+
+def parseBags(bagsStr):
+    bags = {}
+    for line in bagsStr:
+        words = line.split(" ")
+        color = words[0] + " " + words[1]
+        bags[color] = []
+        contains = " ".join(words[4:])
+        for contain in contains.split(", "):
+            containWords = contain.split(" ")
+            containAmount = containWords[0] 
+            containColor = containWords[1] + " " + containWords[2]
+            bags[color].append({
+                "color": containColor,
+                "amount": containAmount
+            })
+    return bags
+
+def checkBag(bag,targetColor, bags):
+    if bag == "other bags.":
+        return 0
+    if bag == targetColor:
+        return 1
+    else:
+        colorsInsideThisBag = [content["color"] for content in bags[bag]]
+        ok = []
+        #recursion WAYTOODANK
+        for color in colorsInsideThisBag:
+            ok.append(checkBag(color, targetColor, bags))
+        return max(ok)
+bags = parseBags(bags)
+res = 0
+targetBag = "shiny gold"
+for k,v in bags.items():
+    if k != targetBag:
+        res += checkBag(k, targetBag, bags)
+
+print(res)

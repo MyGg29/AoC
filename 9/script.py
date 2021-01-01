6891d96847e84e90bb4928b77be0e9be3b8779a6
MyGg29 <gwendalgourmelon@gmail.com>
2021-01-01 14:50:58 +0100

    day 9 part two

--- 9/script.py
+++ 9/script.py
@@ -2,6 +2,7 @@ with open("in") as fIn:
    numbers = fIn.read().split("\n")
 numbers = list(map(lambda a: int(a), numbers))
 
+res = 0
 preambleSize = 25
 slidingList = []
 for number in numbers:
@@ -22,5 +23,19 @@ for number in numbers:
             slidingList.pop(0)
             slidingList.append(number)
         else:
-            print(number)
+            res = number
             break
+print(res)
+
+#part two
+target = res
+continuousSum = []
+i = 0
+while target != sum(continuousSum):
+    continuousSum.append(numbers[i])
+    while sum(continuousSum)> target:
+        continuousSum.pop(0)
+    i += 1
+
+res = min(continuousSum) + max(continuousSum)
+print(res)

550a20b2baf5246c2604db05262a619d684ef75b
MyGg29 <gwendalgourmelon@gmail.com>
2021-01-02 14:51:47 +0100

    day 11 part two

--- 11/script.py
+++ 11/script.py
@@ -12,10 +12,6 @@ CORNER_INCLUDED = [
     [ 1,-1],[ 1, 0],[ 1, 1],
 ]
 
-def isSeatEmpty(x,y, seats):
-    return seats[y][x] == "L"
-
-
 def getAdjacentSeats(x,y,seats):
     adjacentCells = []
     for offsetRowCol in CORNER_INCLUDED:
@@ -40,16 +36,56 @@ def fourOrMoreAdjacentSeatsOccupied(x,y,seats):
     else:
         return False
 
-def doRound(seats):
+#part two
+def getVisibleSeats(x,y,seats):
+    visibleCells = []
+    for offsetRowCol in CORNER_INCLUDED:
+        newX = x
+        newY = y
+        while newY < len(seats) and newY >= 0 and newX < len(seats[0]) and newX >= 0: 
+            newY = newY + offsetRowCol[1]
+            newX = newX + offsetRowCol[0]
+            if newY >= len(seats) or newY < 0 or newX >= len(seats[0]) or newX < 0:
+                continue
+            cell = seats[newY][newX]
+            if cell != ".":
+                visibleCells.append(cell)
+                #dont look further for seat, the person can only see the first one
+                break 
+    return visibleCells
+
+def noVisibleOccupiedSeats(x,y,seats):
+    visibleSeats = getVisibleSeats(x,y,seats)
+    if "#" in visibleSeats:
+        return False
+    else:
+        return True
+
+def fiveOrMoreVisibleSeatsOccupied(x,y,seats):
+    visibleSeats = getVisibleSeats(x,y,seats)
+    if visibleSeats.count("#") >= 5:
+        return True
+    else:
+        return False
+
+def doRound(seats, type):
     resSeats = deepcopy(seats)
     for y,row in enumerate(seats):
         for x,seat in enumerate(row):
             if seat == ".":
                 continue
-            if noAdjacentOccupiedSeats(x,y,seats):
-                resSeats[y][x] = "#"
-            elif fourOrMoreAdjacentSeatsOccupied(x,y,seats):
-                resSeats[y][x] = "L"
+            if type == "ADJACENT":
+                if noAdjacentOccupiedSeats(x,y,seats):
+                    resSeats[y][x] = "#"
+                elif fourOrMoreAdjacentSeatsOccupied(x,y,seats):
+                    resSeats[y][x] = "L"
+            elif type == "VISIBLE":
+                if noVisibleOccupiedSeats(x,y,seats):
+                    resSeats[y][x] = "#"
+                elif fiveOrMoreVisibleSeatsOccupied(x,y,seats):
+                    resSeats[y][x] = "L"
+            else:
+                print("error")
     return resSeats
 
 def printSeats(seats):
@@ -67,6 +103,15 @@ previousState = []
 currentState = seats
 while previousState != currentState:
     previousState = deepcopy(currentState)
-    currentState = doRound(currentState)
+    currentState = doRound(currentState, "ADJACENT")
+
+print(countOccupiedSeats(currentState))
+
+#part two
+previousState = []
+currentState = seats
+while previousState != currentState:
+    previousState = deepcopy(currentState)
+    currentState = doRound(currentState, "VISIBLE")
 
 print(countOccupiedSeats(currentState))
